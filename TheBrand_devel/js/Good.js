bb8a1cf0abd371ea8a20b296ad410d844eabfcf7
DenisSychev <denisvsychev@gmail.com>
2018-03-21 18:04:22 +0300

    Исправлена ошибка отрисовки товаров на странице
    + попытка отрисовывать кнопку не <a>, но <button>

--- TheBrand_devel/js/Good.js
+++ TheBrand_devel/js/Good.js
@@ -1,7 +1,7 @@
 "use strict";
 
 function Good(id, imgSrc, title, price) {
-  Container.call(this.id);
+  Container.call(this, id);
   this.imgSrc = imgSrc;
   this.title = title;
   this.price = price
@@ -11,39 +11,63 @@ Good.prototype = Object.create(Container.prototype);
 Good.prototype.constructor = Good;
 
 Good.prototype.render = function (jQuerySelector) {
+
+  /**
+   * Секция товара
+   * @type {jQuery|HTMLElement}
+   */
   var $goodContainer = $('<section />', {
-    class: 'product',
+    class: 'product'
   });
 
+  /**
+   * Описание товара
+   * @type {jQuery|HTMLElement}
+   */
   var $goodDescription = $('<a />', {
-    href: '#',
+    href: 'single_page.html',
     class: 'descProduct'
   });
 
+  /**
+   * Картинка товара
+   * @type {jQuery|HTMLElement}
+   */
   var $goodImg = $('<img />', {
     src: this.imgSrc,
     alt: this.title
   });
 
+  /**
+   * Название товара
+   * @type {jQuery|HTMLElement}
+   */
   var $goodTitle = $('<h2 />', {
     class: 'desc',
     text: this.title
   });
 
-  var $goodPrice = $('<p class="price">' + this.price + '</p>');
-
   /**
-   * Кнопка добавить в корзину
+   * Цена товара
    * @type {jQuery|HTMLElement}
    */
-  var $goodAddToCart = $('<a />', {
+  var $goodPrice = $('<p />', {
+    class: "price",
+    text: '$' + this.price
+  });
+
+  /**
+   * Кнопка добавить в корзину. Скрыта до наведения указателя на товар
+   * @type {jQuery|HTMLElement}
+   */
+  var $goodAddToCart = $('<button />', {
     class: 'cartProduct',
     'data-id': this.id,
     text: 'Add to Cart'
   });
 
   /**
-   * Добавление иконки корзины и набора Fontello
+   * Корзина и набора Fontello
    * @type {jQuery|HTMLElement}
    */
   var $iconBasket = $('<i />', {
@@ -51,14 +75,18 @@ Good.prototype.render = function (jQuerySelector) {
     'aria-hidden': 'true'
   });
 
+  //Формирование блока Описание товара
   $goodImg.appendTo($goodDescription);
   $goodTitle.appendTo($goodDescription);
   $goodPrice.appendTo($goodDescription);
 
+  //Добавление корзины из Fontello в кнопку
   $iconBasket.appendTo($goodAddToCart);
 
+  //Добавление блока Описание товара в Секцию товара
   $goodDescription.appendTo($goodContainer);
+  //Добавление Кнопки добавить в корзину в Секцию товара
   $goodAddToCart.appendTo($goodContainer);
-
+  //Добавление Секции товара в HTML
   $goodContainer.appendTo(jQuerySelector);
 };
--- TheBrand_devel/js/main.js
+++ TheBrand_devel/js/main.js
@@ -1,8 +1,8 @@
 "use strict";
 
 $(document).ready(function () {
-    // var good1 = new Good('123', 'img/products/product-9.jpg', 'Mango People T-shirt', '52.00');
-    // good1.render($('.items'));
+    var good = new Good('123', 'img/products/product-9.jpg', 'Mango People T-shirt', '52.00');
+    good.render($('.items'));
 
     var basket = new Basket('shopping_cart');
     basket.render($('#shopping_cart'));
